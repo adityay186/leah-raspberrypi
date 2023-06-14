fd1e0586a8e7ab852a70e527ccb9ff9912b3d209
adityay186 <adityay186@gmail.com>
2023-06-14 20:47:39 +0530

    added voice acknowledgements to wake word file

--- tools/check_internet.py
+++ tools/check_internet.py
@@ -8,9 +8,9 @@ def check_internet_connection():
             # Attempt to create a socket and connect to www.google.com
             socket.create_connection(("www.google.com", 80))
             print("Internet connection is available.")
-            os.system("mpg123 connected_successfully.mp3")
+            os.system("mpg123 /home/leah/Documents/leah-final/tools/connected_successfully.mp3")
             break  # Break out of the loop if connection is successful
         except OSError:
             print("No internet connection. Retrying in 5 seconds...")
-            os.system("mpg123 no_internet.mp3")
+            os.system("mpg123 /home/leah/Documents/leah-final/tools/no_internet.mp3")
             time.sleep(5)  # Wait for 5 seconds before retrying
