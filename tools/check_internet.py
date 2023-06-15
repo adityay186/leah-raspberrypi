7e8d429b60fa76bb5c4a89dcc842cbd640152bd5
adityay186 <adityay186@gmail.com>
2023-06-15 11:44:14 +0530

    added few tweaks into loggin into the terminal

--- tools/check_internet.py
+++ tools/check_internet.py
@@ -1,6 +1,6 @@
 import time
 import socket
-import os
+from mpg123_player import play_mpg123
 
 def check_internet_connection():
     while True:
@@ -8,9 +8,9 @@ def check_internet_connection():
             # Attempt to create a socket and connect to www.google.com
             socket.create_connection(("www.google.com", 80))
             print("Connected to the internet.")
-            os.system("mpg123 /home/leah/Documents/leah-final/tools/connected_successfully.mp3")
+            play_mpg123("/home/leah/Documents/leah-final/tools/connected_successfully.mp3")
             break  # Break out of the loop if connection is successful
         except OSError:
             print("No internet connection. Retrying in 5 seconds...")
-            os.system("mpg123 /home/leah/Documents/leah-final/tools/no_internet.mp3")
+            play_mpg123("/home/leah/Documents/leah-final/tools/no_internet.mp3")
             time.sleep(5)  # Wait for 5 seconds before retrying
--- tools/mpg_player.py
+++
@@ -1,4 +0,0 @@
-import os
-
-def play_mpg123(file_path):
-    os.system("mpg123 {} > /dev/null 2>&1".format(file_path))
