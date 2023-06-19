645f494a7317c77f4a0662c6d746d9c99f842804
adityay186 <adityay186@gmail.com>
2023-06-19 21:26:07 +0530

    added play music intent and skill

--- skills/weather.py
+++ skills/weather.py
@@ -1,16 +1,18 @@
 import requests
 
 def get_weather(city):
-    params = {
-      'access_key': '4fc85ccf51de68cc4bd43405d678cd4a',
-      'query': city["location"]
-    }
-    api_result = requests.get('http://api.weatherstack.com/current', params)
-    api_response = api_result.json()
-    res = "In " + str(api_response['location']['name']) + ',' \
-    + str(api_response['location']['region']) + " its " \
-    + str(api_response['current']['temperature']) \
-    + " degrees" \
-    + " and " + str(api_response["current"]["weather_descriptions"][0])
-    
-    return res
+    api_key = '182a87f539d9bca49af1ae23ac08e611'  # Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
+    query = city['location']
+
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={query}&appid={api_key}&units=metric'
+    response = requests.get(url)
+    weather_data = response.json()
+
+    city_name = weather_data['name']
+    region = weather_data['sys']['country']
+    temperature = weather_data['main']['temp']
+    weather_description = weather_data['weather'][0]['description']
+
+    result = f"In {city_name}, {region}, it's {temperature} degrees Celsius and {weather_description}."
+
+    return result
